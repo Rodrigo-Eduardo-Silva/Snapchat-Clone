b7743f687ccfa37ecf00887927612c2335a4ceb0
Rodrigo <rodrigoeduardosilv@gmail.com>
2022-06-07 22:09:16 -0300

    add a new user in Firebase database

--- Snapchat Clone/Controllers/AddNewAccountViewController.swift
+++ Snapchat Clone/Controllers/AddNewAccountViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
 
 class AddNewAccountViewController: UIViewController {
     
     @IBOutlet weak var newAddEmailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var ConfirmPasswordText: UITextField!
-    @IBOutlet weak var userNane: UITextField!
+    @IBOutlet weak var userName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,14 +22,19 @@ class AddNewAccountViewController: UIViewController {
     }
     
     @IBAction func AddNewAccount(_ sender: Any) {
-        if let email = self.newAddEmailText.text , let password = self.passwordText.text, let confirmPassword  = self.ConfirmPasswordText.text {
+        if let email = self.newAddEmailText.text , let password = self.passwordText.text, let confirmPassword  = self.ConfirmPasswordText.text , let newAccountUserName = userName.text  {
             if password == confirmPassword {
                 let authentication = Auth.auth()
+                if newAccountUserName != ""{
                 authentication.createUser(withEmail: email, password: password) { user, error in
                     if error == nil{
                         if user == nil{
-                            
+                            self.showLoginError(message: "Erro ao autenticar o usuário, tente novamente")
                         }else{
+                            let database = Database.database().reference()
+                            let users = database.child("usuários")
+                            let userData = ["nome": newAccountUserName , "email": email]
+                            users.child(user!.user.uid).setValue(userData)
                             self.performSegue(withIdentifier: "addNewAcountSegue", sender: nil)
                         }
                     }else{
@@ -38,13 +44,14 @@ class AddNewAccountViewController: UIViewController {
                     }
                 }
                 print ( "Ok Next step")
+                }else{
+                    let alert = Altert(title: "Dados Incorretos", message: "Digite o seu nome para prosseguir!")
+                    self.present(alert.getAltert(), animated: true, completion: nil)
+                }
             }else {
                 self.showLoginError(message: "As senha devem ser iguais")
             }
         }
-        
-        
-        
     }
     
     func showLoginError(message: String) {
